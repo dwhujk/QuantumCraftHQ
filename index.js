29ff86b026f94a65dab0b72ee2393c4a798202b4
dwhujk <gs1hickup64@hotmail.com>
2024-05-10 03:13:04 +0700

    commit

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-function letterCombinations(digits) {
-  if (digits.length === 0) return [];
-  const map = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
+function findOrder(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  for (const [course, pre] of prerequisites) {
+    graph[pre].push(course);
+    inDegree[course]++;
+  }
+  const queue = [];
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) queue.push(i);
+  }
   const result = [];
-  backtrack("", 0);
-  return result;
-  function backtrack(current, index) {
-    if (current.length === digits.length) {
-      result.push(current);
-      return;
-    }
-    const letters = map[digits[index]];
-    for (const letter of letters) {
-      backtrack(current + letter, index + 1);
+  while (queue.length) {
+    const course = queue.shift();
+    result.push(course);
+    for (const nextCourse of graph[course]) {
+      inDegree[nextCourse]--;
+      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
     }
   }
+  return result.length === numCourses ? result : [];
 }
