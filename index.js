e2164dd23c095434f0675a497a6c662cc86a8880
dwhujk <gs1hickup64@hotmail.com>
2024-05-10 21:14:15 +0700

    demo project

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-function findOrder(numCourses, prerequisites) {
-  const graph = new Array(numCourses).fill(0).map(() => []);
-  const inDegree = new Array(numCourses).fill(0);
-  for (const [course, pre] of prerequisites) {
-    graph[pre].push(course);
-    inDegree[course]++;
-  }
-  const queue = [];
-  for (let i = 0; i < numCourses; i++) {
-    if (inDegree[i] === 0) queue.push(i);
-  }
-  const result = [];
-  while (queue.length) {
-    const course = queue.shift();
-    result.push(course);
-    for (const nextCourse of graph[course]) {
-      inDegree[nextCourse]--;
-      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
-    }
-  }
-  return result.length === numCourses ? result : [];
+function add(x, y) {
+  return x + y;
 }
+
+const result = add(5, 3);
+console.log(result); // 8
