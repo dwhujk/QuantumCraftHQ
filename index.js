08c5c3ed5ba5a38f1ef10843ecbc0b7cb329d531
dwhujk <gs1hickup64@hotmail.com>
2024-05-11 23:30:56 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function add(x, y) {
-  return x + y;
-}
-
-const result = add(5, 3);
-console.log(result); // 8
+const factorialRecursive = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorialRecursive(n - 1);
+};
