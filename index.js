0b9de6708722e798ba0502ce1ab70c9f2c827974
dwhujk <gs1hickup64@hotmail.com>
2024-05-13 15:22:55 +0700

    update commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorialRecursive(n - 1);
-};
+const reverseString = (str) => str.split("").reverse().join("");
